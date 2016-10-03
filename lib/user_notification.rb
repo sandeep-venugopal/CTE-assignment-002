60bd789e0918f9dd53ced1634c5e3eaa80918834
Sandeep <sandeep.mkv@gmail.com>
2016-10-04 01:26:00 +0530

    Implement UserNotification class - Task1

---
+++ lib/user_notification.rb
@@ -0,0 +1,46 @@
+class UserNotification
+  attr_reader :user_id, :message, :message_type, :pop_up, :publish_at, :target,
+              :device_ids
+  def initialize(payload)
+    return if payload.nil?
+    @properties = payload.fetch('properties', {})
+    @payload = payload.fetch('payload', {})
+    build_from_properties unless @properties.empty?
+    unless @payload.empty?
+      set_message_properties
+      set_target_and_device_ids
+    end
+  end
+
+  private
+
+  def build_from_properties
+    return if @properties.empty?
+    @user_id = @properties.fetch('user_id', '')
+    @publish_at = @properties.fetch('effective_date', '')
+  end
+
+  def set_message_properties
+    options = @payload.fetch('options', {})
+    return if options.empty?
+    alert_message = options.fetch('alert_message', '')
+    badge_count = options.fetch('badge_count', '')
+    silent = options.fetch('silent', true)
+    silent ? @message = alert_message : @message = alert_message + badge_count
+    @message_type = options.fetch('notification_type', '')
+    @pop_up = options.fetch('silent', true)
+  end
+
+  def set_target_and_device_ids
+    options = @payload.fetch('options', {})
+    return if options.empty?
+    if !options['devices'].fetch('ios', nil).nil?
+      @target = 'apn'
+      token = options['devices']['ios']
+    else
+      @target = 'gcm'
+      token = options['devices']['android']
+    end
+    @device_ids = token
+  end
+end
---
+++ test/task1_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+require 'user_notification'
+
+class Task1Test < Minitest::Test
+  def test_that_it_can_be_initialized_from_a_payload_hash
+    assert UserNotification.new({})
+  end
+
+  def test_that_it_constructs_the_object_correctly_from_the_given_initalization_payload
+    device_id = 'e25454608b6097bc412be42ad9bf39797a698925d947b9d136cbb992f649cc96'
+    payload = {
+      'properties' => {
+        'user_id' => 43,
+        'managing_user_id' => 43,
+        'description' => 'Push Notification',
+        'effective_date' => '2015-07-20T06:28:36-05:00',
+        'system_date' => '2015-07-20T06:28:36-05:00',
+      },
+      'payload' => {
+        'id' => 49,
+        'options' => {
+          'devices' => {
+            'ios' => device_id
+          },
+          'alert_message' => 'You have following notifications',
+          'badge_count' => 10,
+          'created_at' => '2015-07-20T06:28:36-05:00',
+          'id' => 48,
+          'member_id' => 25,
+          'notification_type' => 'secure_message',
+          'silent' => true,
+          'updated_at' => '2015-07-20T06:28:36-05:00'
+        }
+      }
+    }
+    user_notification = UserNotification.new(payload)
+    assert_equal 43, user_notification.user_id
+    assert user_notification.pop_up
+    assert_equal 'You have following notifications', user_notification.message
+    assert_equal 'secure_message', user_notification.message_type
+    assert_equal '2015-07-20T06:28:36-05:00', user_notification.publish_at
+    assert_equal 'apn', user_notification.target
+    assert_equal device_id, user_notification.device_ids
+  end
+
+  def test_that_it_does_not_throw_error_when_empty_payload_is_given
+    assert UserNotification.new({})
+    assert UserNotification.new(nil)
+  end
+end
